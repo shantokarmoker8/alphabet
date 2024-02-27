d373b6c81233fda705111918452dc558f911e769
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-02-27 12:42:15 +0600

    js playNow

---
+++ script/main.js
@@ -0,0 +1,6 @@
+function playNow() {
+  const homeSection = document.getElementById("home");
+  const playGroundSection = document.getElementById("play-ground");
+  homeSection.classList.add("hidden");
+  playGroundSection.classList.remove("hidden");
+}
